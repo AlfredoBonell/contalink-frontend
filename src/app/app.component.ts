a78301d3e4bf75822fb844fd497edb01bef94035
Alfredo Bonell <99234057+AlfredoBonell@users.noreply.github.com>
2022-08-25 09:17:44 -0500

    Contalink consumo API
    - Se realiza petición y consumo de API con la extracción de la información.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,19 @@ import * as _moment from 'moment';
 import { Moment } from 'moment';
 
 const moment = _moment;
-const regExp = new RegExp("([0-9]{4}[-](0[1-9]|1[0-2])[-]([0-2]{1}[0-9]{1}|3[0-1]{1})|([0-2]{1}[0-9]{1}|3[0-1]{1})[-](0[1-9]|1[0-2])[-][0-9]{4})");
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   range = new FormGroup({
     start: new FormControl<Date | null>(null),
     end: new FormControl<Date | null>(null),
   });
-  
+
   title = 'contalink-frontend';
 
   fechaInicio!: string;
@@ -33,28 +32,26 @@ export class AppComponent implements OnInit{
   startChange(event: { value: any; }) {
     this.fechaInicio = moment(event.value, "DD/MM/YYYY").format("YYYY-MM-DD");
     console.log("La fecha inicio es: " + this.fechaInicio);
-
-    if (!regExp.test(this.fechaInicio)) {
-      alert("La fecha inicio es incorrecta");
-    }
-
   }
 
   endChange(event: { value: any; }) {
     this.fechaFin = moment(event.value, "DD/MM/YYYY").format("YYYY-MM-DD");
     console.log("La fecha final es: " + this.fechaFin);
 
-    if (!regExp.test(this.fechaFin)) {
-      alert("La fecha final es incorrecta");
-    }
-
     this.restService.getReporte(this.fechaInicio, this.fechaFin).subscribe(
-      (res)=>{
-        console.log(res);
-        this.invoices = res;
+      (res) => {
+        if (res.invoicesList.length > 0) {
+          this.invoices = res.invoicesList;
+          console.log(res);
+        }
+        else {
+          alert("No hay facturas para mostrar");
+          this.invoices = res.invoicesList;
+        }
       },
-      (err)=>{
+      (err) => {
         console.log(err);
+        alert("Error al obtener las facturas");
       }
     )
   }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Invoice } from './Invoice';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +12,9 @@ export class RestService {
 
   getReporte(fechaInicio : string, fechaFin : string) {
 
-    const formData = new FormData();
-    formData.append('fechaInicio', fechaInicio);
-    formData.append('fechaFin', fechaFin);
-
-    return this.http.post<Invoice[]>(this.url, formData);
+    return this.http.post<any>(this.url, {
+      fechaInicio: fechaInicio,
+      fechaFin: fechaFin
+    });
   }
 }
